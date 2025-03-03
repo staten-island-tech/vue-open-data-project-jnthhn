8d1a97d7bb74522076ac37ba1bcf538f2961c1f9
jnthhn <jonathanh321@nycstudents.net>
2025-03-03 12:28:32 -0500

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div></div>
 </template>
+<script setup>
+import { ref, onMounted } from 'vue'
+const arrest_key = ref('')
+async function getArrest() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/8h9b-rp9u.json')
+  let data = await res.json()
+  arrest_key.value = data.results
+}
+onMounted(() => {
+  getArrest()
+})
+
+
+<style scoped></style>
